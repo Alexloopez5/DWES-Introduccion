fa74bec45c75fb9092e9d3c9a86bcb1a3fd30c0c
Alex Lopez <lopeznavarroalex5@gmail.com>
2023-09-27 10:01:29 +0200

    Acabado el ejercicio de imagenes

---
+++ imagen.php
@@ -0,0 +1,15 @@
+<?php
+    header('Content-Type: image/png');
+    $letMeIn = $_GET['letMeIn'] ?? 0;
+    $imagen = $_GET['img'];
+
+    if($letMeIn == 1 && file_exists("/home/alumno/DWES/Imagenes/" . $imagen)){
+        imagepng(imagecreatefrompng("/home/alumno/DWES/Imagenes/" . $imagen));
+    }elseif ($letMeIn == 0){
+        http_response_code(401);
+        imagepng(imagecreatefrompng("/home/alumno/DWES/Imagenes/prohibido.png"));
+    }elseif ($letMeIn == 1 && !file_exists("/home/alumno/DWES/Imagenes/". $imagen)){
+        http_response_code(404);
+        imagepng(imagecreatefrompng("/home/alumno/DWES/Imagenes/prohibido.png"));
+    }
+?>
