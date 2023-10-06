9ce14b73b230407f6a761f9b0a23f3e34052b783
Alex Lopez <lopeznavarroalex5@gmail.com>
2023-10-06 09:47:46 +0200

    Inicio de sesion modificado

--- Sesiones/privada.php
+++ Sesiones/privada.php
@@ -1,8 +1,14 @@
 <?php 
 session_start();
+function filtrado($datos){
+    $datos = trim($datos); // Elimina espacios antes y después de los datos
+    $datos = stripslashes($datos); // Elimina backslashes 
+    $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
+    return $datos;
+}
 if (!isset($_SESSION['username'])) {
     $_SESSION['msg'] = "Para acceder a esta página, debes iniciar sesión primero";
-    header('location: login.php?returnToUrl=galeria.php');
+    header('location: login.php');
 }
 ?>
 
